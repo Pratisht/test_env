c81d1d25206909d522f01846a81ac8557f9bd17c
Pratisht <55251107+Pratisht@users.noreply.github.com>
2023-03-21 12:14:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import importlib
 # specify the name of the module to be imported (this could be modified)
 module_name = "example_module"
 
+
 # dynamically import the module using importlib 
 my_module = importlib.import_module(module_name)
 
